21397c13c7c8e65e136ff5fe7cd388dc171cd9ae
DevaK25 <devanggakertawijaya@gmail.com>
2021-11-30 16:37:14 +0700

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, react</h1>
-    </div>
+    <> 
+      <Header>
+
+      </Header>
+      <main>
+        <h1>Welcome to My Shop</h1>
+      </main>
+      <Footer>
+        
+      </Footer>
+    </>
   );
 }
 
